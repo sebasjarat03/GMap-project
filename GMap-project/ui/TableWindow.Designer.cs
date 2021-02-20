de677f30e2f31f0c4d6beea6668273994623873d
Sebastian Jaramillo <sebastian.jaramillo2@correo.icesi.edu.co>
2021-02-20 18:58:49 -0500

    Filters on table now working

--- GMap-project/ui/TableWindow.Designer.cs
+++ GMap-project/ui/TableWindow.Designer.cs
@@ -30,6 +30,10 @@ namespace GMap_project.ui
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fieldBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.parameterBox = new System.Windows.Forms.ComboBox();
+            this.placeName = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,29 +42,76 @@ namespace GMap_project.ui
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(610, 426);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(813, 524);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
+            // fieldBox
+            // 
+            this.fieldBox.FormattingEnabled = true;
+            this.fieldBox.Location = new System.Drawing.Point(882, 40);
+            this.fieldBox.Name = "fieldBox";
+            this.fieldBox.Size = new System.Drawing.Size(270, 24);
+            this.fieldBox.TabIndex = 1;
+            this.fieldBox.SelectedIndexChanged += new System.EventHandler(this.fieldBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(882, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Choose a field:";
+            // 
+            // parameterBox
+            // 
+            this.parameterBox.FormattingEnabled = true;
+            this.parameterBox.Location = new System.Drawing.Point(885, 108);
+            this.parameterBox.Name = "parameterBox";
+            this.parameterBox.Size = new System.Drawing.Size(267, 24);
+            this.parameterBox.TabIndex = 3;
+            this.parameterBox.Visible = false;
+            this.parameterBox.SelectedIndexChanged += new System.EventHandler(this.parameterBox_SelectedIndexChanged);
+            // 
+            // placeName
+            // 
+            this.placeName.Location = new System.Drawing.Point(885, 108);
+            this.placeName.Name = "placeName";
+            this.placeName.Size = new System.Drawing.Size(253, 22);
+            this.placeName.TabIndex = 4;
+            this.placeName.Visible = false;
+            // 
             // TableWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(974, 450);
+            this.ClientSize = new System.Drawing.Size(1299, 554);
+            this.Controls.Add(this.placeName);
+            this.Controls.Add(this.parameterBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fieldBox);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "TableWindow";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-
+        private System.Windows.Forms.ComboBox fieldBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox parameterBox;
+        private System.Windows.Forms.TextBox placeName;
     }
 }
--- GMap-project/ui/TableWindow.cs
+++ GMap-project/ui/TableWindow.cs
@@ -12,15 +12,89 @@ namespace GMap_project.ui
 {
     public partial class TableWindow : Form
     {
+        private static String GESTOR = "CATEGORICO";
+        private static String MUNICIPIO = "CATEGORICO";
+        private static String NOMBRE_DEL_SITIO = "CADENA";
+
+        List<String> municipios = new List<string>();
+        List<String> gestores = new List<string>();
+
         public TableWindow(DataTable data)
         {
             InitializeComponent();
             this.dataGridView1.DataSource = data;
+            for (int i = 0; i < dataGridView1.ColumnCount; i++) {
+                if(i==0 || i== 2 || i == 4) {
+                    String fields = dataGridView1.Columns[i].Name;
+                    fieldBox.Items.Add(fields);
+                }
+                
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                gestores.Add(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                municipios.Add(dataGridView1.Rows[i].Cells[2].Value.ToString());
+
+            }
+            
+
+
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void fieldBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (fieldBox.Text.Equals("GESTOR"))
+            {
+                parameterBox.Visible = true;
+                placeName.Visible = false;
+                parameterBox.Items.Clear();
+                var control = "";
+                
+                gestores.Sort();
+                for (int i = 0; i < gestores.Count; i++) {
+                    String gestor = gestores[i];
+                    if (!gestor.Equals(control))
+                    {
+
+                        parameterBox.Items.Add(gestor);
+                        control = gestor;
+                    }
+                }
+            }
+            else if (fieldBox.Text.Equals("MUNICIPIO"))
+            {
+                parameterBox.Visible = true;
+                placeName.Visible = false;
+                parameterBox.Items.Clear();
+                var control = "";
+                
+
+                municipios.Sort();
+                for (int i = 0; i < municipios.Count; i++) {
+                    String municipio = municipios[i];
+                    if (!municipio.Equals(control))
+                    {
+                        parameterBox.Items.Add(municipio);
+                        control = municipio;
+                    }
+                }
+                
+            }
+            else {
+                placeName.Visible = true;
+                parameterBox.Visible = false;
+            }
+        }
+
+        private void parameterBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string filterField = fieldBox.Text;
+            ((DataTable)dataGridView1.DataSource).DefaultView.RowFilter = string.Format("[{0}] LIKE '{1}'", filterField, parameterBox.Text);
         }
     }
 }
